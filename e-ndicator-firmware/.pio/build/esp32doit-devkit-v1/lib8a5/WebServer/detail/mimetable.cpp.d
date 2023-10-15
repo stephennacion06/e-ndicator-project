c47e10f195bce46a4af9c85693ee964e976e73b0
Stephen Nacion <stephennacion30@gmail.com>
2023-10-15 20:25:26 +0800

    added .pio files

---
+++ e-ndicator-firmware/.pio/build/esp32doit-devkit-v1/lib8a5/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32doit-devkit-v1/lib8a5/WebServer/detail/mimetable.cpp.o: \
+ C:/Users/Stephen/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ C:/Users/Stephen/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ C:/Users/Stephen/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
